fc127a9a5e785202be6cdfb8221d5f4f53757ecc
kapinga <erickfloriana@gmail.com>
2025-05-06 21:53:14 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+    public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            TaskManager manager = new TaskManager();
+            manager.setVisible(true);
+        });
+    }
+}
+
+    
+
